021a3922c8d4f8714191d3ec1edf11da90fa0581
Egor Talovikov <egtalovikov@gmail.com>
2023-08-12 00:49:29 +0500

    feat: добавил лоадер

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {Button, Container, Grid} from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 
 const Loader = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Grid container
+                  style={{height: window.innerHeight - 50}}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+            >
+                <Grid
+                      container
+                      alignItems={"center"}
+                      direction={"column"}
+                >
+                    <div className="lds-hourglass"></div>
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
