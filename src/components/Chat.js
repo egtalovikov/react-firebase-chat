7348af89279c02b9456468916f950843e0afa052
Egor Talovikov <egtalovikov@gmail.com>
2023-08-12 00:57:49 +0500

    feat: сверстал контейнер для чата

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Context} from "../index";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {Container, Grid, TextField} from "@material-ui/core";
 
 const Chat = () => {
+    const {auth, firestore} = useContext(Context)
+    const [user] = useAuthState(auth)
+
     return (
-        <div>
-            chat
-        </div>
+        <Container>
+            <Grid container
+                  justifyContent={"center"}
+                  style={{height: window.innerHeight - 50}}>
+                <div style={{width: '80%', height: '70vh', border: '1px solid gray', overflowY: 'auto'}}>
+                    <Grid
+                        container
+                        direction={"column"}
+                        alignItems={"flex-end"}
+                        style={{width: '80%'}}
+                    >
+                    </Grid>
+                </div>
+            </Grid>
+        </Container>
     );
 };
 
