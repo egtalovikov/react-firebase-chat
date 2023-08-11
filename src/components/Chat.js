f3237276f322809fe7d8c6045474c18e16f4811f
Egor Talovikov <egtalovikov@gmail.com>
2023-08-12 01:01:09 +0500

    feat: сверстал кнопки и формы чата

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
 import {useAuthState} from "react-firebase-hooks/auth";
-import {Container, Grid, TextField} from "@material-ui/core";
+import {Button, Container, Grid, TextField} from "@material-ui/core";
 
 const Chat = () => {
     const {auth, firestore} = useContext(Context)
@@ -11,16 +11,22 @@ const Chat = () => {
         <Container>
             <Grid container
                   justifyContent={"center"}
-                  style={{height: window.innerHeight - 50}}>
-                <div style={{width: '80%', height: '70vh', border: '1px solid gray', overflowY: 'auto'}}>
-                    <Grid
-                        container
-                        direction={"column"}
-                        alignItems={"flex-end"}
-                        style={{width: '80%'}}
-                    >
-                    </Grid>
+                  style={{height: window.innerHeight - 50, marginTop: 20}}>
+                <div style={{width: '80%', height: '50vh', border: '1px solid gray', overflowY: 'auto'}}>
                 </div>
+                <Grid
+                    container
+                    direction={"column"}
+                    alignItems={"flex-end"}
+                    style={{width: '80%'}}
+                >
+                    <TextField
+                        fullWidth
+                        rowsMax={2}
+                        variant={"outlined"}
+                    />
+                    <Button variant={"outlined"}>Отправить</Button>
+                </Grid>
             </Grid>
         </Container>
     );
