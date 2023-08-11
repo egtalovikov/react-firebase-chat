d9065ab7d7167035f23c47d5d63ed0e171738a4b
Egor Talovikov <egtalovikov@gmail.com>
2023-08-11 21:37:27 +0500

    feat: создал AppBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
+import { AppBar } from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
 import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <AppBar color={"secondary"} position="static">
+            <Toolbar>
+            </Toolbar>
+        </AppBar>
     );
 };
 
