9d13d397627a6b86b12555b54b08e4e1c94a95cc
Egor Talovikov <egtalovikov@gmail.com>
2023-08-11 21:56:08 +0500

    feat: сверстал Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
-import { AppBar } from '@mui/material';
-import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@material-ui/core/AppBar';
 import React from 'react';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Button, Grid } from '@material-ui/core';
+import { NavLink } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../utils/consts';
+
 
 const Navbar = () => {
+    const user = true
+
     return (
         <AppBar color={"secondary"} position="static">
             <Toolbar>
+                <Grid container justifyContent={"flex-end"}>
+                    {user ?
+                        <Button variant={"outlined"}>Выйти</Button>
+                        :
+                        <NavLink to={LOGIN_ROUTE}>
+                            <Button variant={"outlined"}>Логин</Button>
+                        </NavLink>
+                    }
+                </Grid>
             </Toolbar>
         </AppBar>
     );
