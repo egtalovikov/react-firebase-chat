aa6bf110f419fbee2410c0d052ca85c8a8d8685f
Egor Talovikov <egtalovikov@gmail.com>
2023-08-11 20:42:40 +0500

    feat: добавил роутинг

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+    </BrowserRouter>
   );
 };
 
